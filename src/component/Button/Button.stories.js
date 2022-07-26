08a45bb8112c379a621f3de28102bfc543a0cfb4
nilamanandhar <nila.manandhar@grafioffshorenepal.com>
2022-07-26 10:57:46 +0545

    changes import file and add button storybook

--- src/component/Button/Button.stories.js
+++ src/component/Button/Button.stories.js
@@ -1,21 +1,46 @@
 import React from "react";
-import ButtonBox from "./Button";
 import "bootstrap/dist/css/bootstrap.css";
 import "../../style.modules.scss";
+
+import ButtonBox from "./Button";
+import { CrossIcon } from "../../assets/svg/Logo/Icons";
+
 export default {
   title: "Form/Control/ButtonBox",
   component: ButtonBox,
   onClick: Function,
 };
-export const PrimaryButton = () => {
+export const Button = (args) => {
   return (
     <ButtonBox
       onClick={() => {
         alert("clicked");
       }}
-      className="default-boxStyle"
+      {...args}
     >
-      text
+      {args.children}
     </ButtonBox>
   );
+};
+export const IconButton = (args) => {
+  return (
+    <ButtonBox
+      {...args}
+      onClick={() => {
+        alert("icon Button clicked");
+      }}
+    >
+      {args.children}
+    </ButtonBox>
+  );
+};
+export const _Button = Button.bind({});
+_Button.args = {
+  children: "text",
+  customClass: "default-boxStyle",
+};
+export const _IconButton = IconButton.bind({});
+_IconButton.args = {
+  children: <CrossIcon />,
+  customClass: "default-boxStyle",
 };
