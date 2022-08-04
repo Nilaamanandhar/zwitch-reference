5609e560ce0a79b122f0a78674a40631310fd693
joshinasla <joshinasla18@gmail.com>
2022-08-04 14:06:46 +0545

    win game

--- src/pages/WinGamePage.tsx
+++ src/pages/WinGamePage.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import blueBackground from "../assets/background_img/bluebackground.jpg";
+import MonkeyOneImg from "../assets/background_img/monkeyOne.png";
 
 export default function WinGamePage() {
-  return <div>WinGamePage</div>;
+  return (
+    <div>
+      <img className="background-img" src={blueBackground} />
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <h1 className="text-white text-center">
+          Goed gedaan!
+        </h1>
+        <div className="d-flex justify-content-center">
+          <img src={MonkeyOneImg} className="" />
+        </div>
+      </div>
+    </div>
+  );
 }
