6a197a3124de82c732da58e7ec5f86b28903f838
nilamanandhar <nila.manandhar@grafioffshorenepal.com>
2022-08-29 16:54:07 +0545

    added level of speed2

--- src/pages/LevelTwo.tsx
+++ src/pages/LevelTwo.tsx
@@ -6,6 +6,9 @@ import { WoodBlock } from "../assets/game_blocks/blocks";
 import TopNavbar from "../component/Navbar/navbar";
 import { navbarSlice } from "../redux/navbar/navbar.slice";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import level1 from "../assets/speed2level1.png";
+import level2 from "../assets/speed2level2.png";
+import level3 from "../assets/speed2level3.png";
 
 type LevelTwoType = {
   handleFullScreen: any;
@@ -30,22 +33,25 @@ function LevelTwo(props: LevelTwoType) {
           <img src={MonkeyImg} />
         </div>
         <div className="brick-items">
-          <div className="row">
-            <div className="col" onClick={() => navigate("/level2/sublevel1")}>
+          <div className="row branch-row">
+            <img className="branch-one" src={level1} />
+            <img className="branch-two" src={level2} />
+            <img className="branch-three" src={level3} />
+            {/* <div className="col" onClick={() => navigate("/level2/sublevel1")}>
               <span className="wood">
                 <WoodBlock />
               </span>
-            </div>
-            <div className="col" onClick={() => navigate("/level2/sublevel1")}>
+            </div> */}
+            {/* <div className="col" onClick={() => navigate("/level2/sublevel1")}>
               <span className="wood">
                 <WoodBlock />
               </span>
-            </div>
-            <div className="col" onClick={() => navigate("/level2/sublevel1")}>
+            </div> */}
+            {/* <div className="col" onClick={() => navigate("/level2/sublevel1")}>
               <span className="wood">
                 <WoodBlock />
               </span>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
